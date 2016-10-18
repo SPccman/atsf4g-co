995671844c6cd664f7c1e3fcd8ae75ac99d45fa6
owent <owt5008137@live.com>
2016-10-18 19:13:30 +0800

    增加一些日志，辅助客户端调试

--- install/tools/script/common/project_utils.py
+++ install/tools/script/common/project_utils.py
@@ -3,51 +3,87 @@
 
 import sys
 import os, ctypes, platform
-import cgi
+import cgi, re
 
 
-cmd_opts = None
+global_opts = None
 server_opts = None
 server_name = ''
 server_index = 1
 server_cache_id = None
 server_cache_full_name = None
+server_cache_ip = {}
 
-def set_cmd_opts(opts):
-    cmd_opts = opts
+def set_global_opts(opts):
+    global global_opts
+    global_opts = opts
 
 def set_server_inst(opts, key, index):
+    global server_opts
+    global server_name
+    global server_index
+    global server_cache_id
+    global server_cache_full_name
     server_opts = opts
     server_name = key
     server_index = index
     server_cache_id = None
     server_cache_full_name = None
 
+def get_ip_list_v4():
+    global server_cache_ip
+    if server_cache_ip.ipv4 is None:
+        import socket
+        server_cache_ip.ipv4 = []
+        for ip_pair in socket.getaddrinfo(socket.gethostname(), 0, socket.AF_INET, socket.SOCK_STREAM):
+            server_cache_ip.ipv4.push(ip_pair[4][0])
+    return server_cache_ip.ipv4
+
+def get_ip_list_v6():
+    global server_cache_ip
+    if server_cache_ip.ipv6 is None:
+        import socket
+        server_cache_ip.ipv6 = []
+        for ip_pair in socket.getaddrinfo(socket.gethostname(), 0, socket.AF_INET6, socket.SOCK_STREAM):
+            server_cache_ip.ipv6.push(ip_pair[4][0])
+    return server_cache_ip.ipv6
+
 def get_inner_ipv4():
     if 'SYSTEM_MACRO_INNER_IPV4' in os.environ:
         return os.environ['SYSTEM_MACRO_INNER_IPV4']
     # detect inner ip address
-    return '127.0.0.1'
+    res = get_ip_list_v4()
+    if 0 == len(res):
+        return '127.0.0.1'
+    return res[0]
 
 
 def get_outer_ipv4():
     if 'SYSTEM_MACRO_OUTER_IPV4' in os.environ:
         return os.environ['SYSTEM_MACRO_OUTER_IPV4']
     # detect inner ip address
-    return '0.0.0.0'
+    res = get_ip_list_v4()
+    if 0 == len(res):
+        return '0.0.0.0'
+    return res[0]
 
 def get_inner_ipv6():
     if 'SYSTEM_MACRO_INNER_IPV6' in os.environ:
         return os.environ['SYSTEM_MACRO_INNER_IPV6']
     # detect inner ip address
-    return '::1'
-
+    res = get_ip_list_v6()
+    if 0 == len(res):
+        return '::1'
+    return res[0]
 
 def get_outer_ipv6():
     if 'SYSTEM_MACRO_OUTER_IPV6' in os.environ:
         return os.environ['SYSTEM_MACRO_OUTER_IPV6']
     # detect inner ip address
-    return '::'
+    res = get_ip_list_v6()
+    if 0 == len(res):
+        return '::'
+    return res[0]
 
 def get_hostname():
     if 'SYSTEM_MACRO_HOST_NAME' in os.environ:
@@ -55,37 +91,69 @@ def get_hostname():
     return ''
 
 def get_global_option(section, key, default_val, env_name = None):
+    global global_opts
     if not env_name is None and env_name in os.environ:
         return os.environ[env_name]
     
-    if cmd_opts.has_option(section, key):
-        return cmd_opts.get(section, key)
+    if global_opts.has_option(section, key):
+        return global_opts.get(section, key)
     
     return default_val
 
+def get_global_list(section, key, default_val, env_name = None):
+    res = get_global_option()
+    ret = []
+    for item in res:
+        ret.push(item.strip())
+    return ret
+
+def get_global_list_to_hosts(section, key, default_val, env_name = None):
+    res = get_global_list()
+    ret = []
+    mat = re.compile('([^:]*):(\d+)-(\d+)\s*$')
+    for item in res:
+        mat_res = mat.match(item)
+        if mat_res:
+            for i in range(int(mat_res.group(2)), int(mat_res.group(3)) + 1):
+                ret.push('{0}:{1}'.format(mat_res.group(1), i))
+        else:
+            ret.push(item)
+    return ret
+
 def get_server_name():
+    global server_name
     return server_name
 
 def get_server_option(key, default_val, env_name = None):
     return get_global_option('server.{0}'.format(get_server_name()), key, default_val, env_name)
 
+def get_server_list(key, default_val, env_name = None):
+    return get_global_list('server.{0}'.format(get_server_name()), key, default_val, env_name)
+
+def get_server_list_to_hosts(key, default_val, env_name = None):
+    return get_global_list_to_hosts('server.{0}'.format(get_server_name()), key, default_val, env_name)
+
 def get_server_index():
+    global server_index
     return server_index
 
 def get_server_id():
+    global server_cache_id
+    global global_opts
     if not server_cache_id is None:
         return server_cache_id
 
-    if not cmd_opts.has_option('atservice', get_server_name()):
+    if not global_opts.has_option('atservice', get_server_name()):
         return 0
-    group_id = get_global_option('global', 'group_id', 1)
-    group_step = get_global_option('global', 'group_step', 0x10000)
-    type_step = get_global_option('global', 'type_step', 0x100)
-    type_id = get_global_option('atservice', get_server_name(), 0)
+    group_id = int(get_global_option('global', 'group_id', 1))
+    group_step = int(get_global_option('global', 'group_step', 0x10000))
+    type_step = int(get_global_option('global', 'type_step', 0x100))
+    type_id = int(get_global_option('atservice', get_server_name(), 0))
     server_cache_id = group_id * group_step + type_step * type_id + get_server_index()
     return server_cache_id
 
 def get_server_full_name():
+    global server_cache_full_name
     if not server_cache_full_name is None:
         return server_cache_full_name
     server_cache_full_name = '{0}-{1}'.format(get_server_name(), get_server_index())
--- install/tools/script/gen_conf.py
+++ install/tools/script/gen_conf.py
@@ -5,28 +5,43 @@ import sys
 import os, platform, locale
 import shutil, re, string
 from optparse import OptionParser
+import configparser
 import glob
 import common.print_color
-import common.project_utils
+import common.project_utils as project
 
 console_encoding = sys.getfilesystemencoding()
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
 if __name__ == '__main__':
     os.chdir(script_dir)
+    from mako.template import Template
+    from mako.lookup import TemplateLookup
+    project_lookup = TemplateLookup(directories=[
+        os.path.join(script_dir, 'helper', 'template', 'etc'),
+        os.path.join(script_dir, 'helper', 'template', 'script')
+    ])
 
     parser = OptionParser("usage: %prog [options...]")
     parser.add_option("-e", "--env-prefix", action='store', dest="env_prefix", default='AUTOBUILD_', help="prefix when read parameters from environment variables")
     parser.add_option("-c", "--config", action='store', dest="config", default=os.path.join(script_dir, 'config.conf'), help="configure file(default: {0})".format(os.path.join(script_dir, 'config.conf')))
     parser.add_option("-s", "--set", action='append' , dest="set_vars", default=[], help="set configures")
     parser.add_option("-n", "--number", action='store' , dest="reset_number", default=None, type='int', help="set default server numbers")
+    parser.add_option("-i", "--id-offset", action='store' , dest="server_id_offset", default=0, type='int', help="set server id offset(default: 0)")
+    parser.add_option("-p", "--port-offset", action='store' , dest="server_port_offset", default=1, type='int', help="set server port offset(default: 0)")
 
     (opts, args) = parser.parse_args()
+    config = configparser.ConfigParser(inline_comment_prefixes=('#', ';'))
+    config.read(opts.config)
+    # all custon environment start with SYSTEM_MACRO_CUSTOM_
     # TODO reset all servers's number
     # TODO set all custom configures
-    set_cmd_opts(opts)
+    project.set_global_opts(config)
     # TODO parse all services
-    for generator in glob.glob(os.path.join(script_dir, 'helper', 'generator', '*')):
-        set_server_inst(None, 'atproxy', 1)
-        # TODO use all template
-        # http://www.makotemplates.org/
+    #for generator in glob.glob(os.path.join(script_dir, 'helper', 'generator', '*')):
+    #    set_server_inst(None, 'atproxy', 1)
+    #    # TODO use all template
+    #    # http://www.makotemplates.org/
+    project.set_server_inst(None, 'atproxy', 1)
+    mytemplate = Template(filename=os.path.join(script_dir, 'helper', 'template', 'etc', 'atproxy.conf'), lookup=project_lookup)
+    print(mytemplate.render())
